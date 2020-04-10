f652a3c69401bc0dc8299e9d83ddb50f4aab6286
Panda-Hero <2635644141@qq.com>
2020-04-10 18:14:31 +0800

    Delete Phase_Calibration_ConjuMulti.m

--- Phase_Calibration_ConjuMulti.m
+++
@@ -1,8 +0,0 @@
-function outPhase = Phase_Calibration_ConjuMulti(inCSI)
-%Using conjugate multiplication to realize phase calibration
-%Input: Complex CSI sequence
-%Output: Calibrated CSI sequence
-    selectedCSI = Antenna_Select(inCSI);
-    outPhase = selectedCSI(1:30,:) .* conj(selectedCSI(end-29:end,:));
-    outPhase = angle(outPhase);
-end
